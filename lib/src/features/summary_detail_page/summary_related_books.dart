48c765174f5310c08b658ed541b1e7d773d8c419
SherazAli-67 <soomrosheraz054@gmail.com>
2025-09-04 12:58:59 +0500

    Backend Integration: Book Detail Page Development
    1. Summary Related Book bugs fixed

--- lib/src/features/summary_detail_page/summary_related_books.dart
+++ lib/src/features/summary_detail_page/summary_related_books.dart
@@ -72,60 +72,65 @@ class _SummaryRelatedBooksState extends State<SummaryRelatedBooks> {
           );
         }
 
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              spacing: 10,
-              children: [
-                Icon(Icons.auto_stories, color: Colors.grey),
-                Text(
-                  'You might also like',
-                  style: AppTextStyles.smallTextStyle.copyWith(fontWeight: FontWeight.w600),
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
-            Column(
-                children: booksProvider.relatedBooks.map((book){
-                  // final book = booksProvider.relatedBooks[index];
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: ListTile(
-                      leading: ClipRRect(
-                          borderRadius: BorderRadius.circular(5),
-                          child: CachedNetworkImage(imageUrl: book.image, )),
-                      title: Text(book.bookName, style: AppTextStyles.smallTextStyle.copyWith(fontWeight: FontWeight.w600),),
-                      subtitle: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        spacing: 10,
-                        children: [
-                          Expanded(child: Text(book.author)),
-                          Container(
-                              padding: EdgeInsets.all(3),
-                              decoration: BoxDecoration(
-                                  color: AppColors.textFieldFillColor.withValues(alpha: 0.7),
-                                  borderRadius: BorderRadius.circular(99)
+        return Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                spacing: 10,
+                children: [
+                  Icon(Icons.auto_stories, color: Colors.grey),
+                  Text(
+                    'You might also like',
+                    style: AppTextStyles.smallTextStyle.copyWith(fontWeight: FontWeight.w600),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              ListView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: booksProvider.relatedBooks.length,
+                itemBuilder: (ctx, index){
+                final book = booksProvider.relatedBooks[index];
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: ListTile(
+                    minLeadingWidth: 45,
+                    leading: ClipRRect(
+                        borderRadius: BorderRadius.circular(5),
+                        child: CachedNetworkImage(imageUrl: book.image,)),
+                    title: Text(book.bookName, style: AppTextStyles.smallTextStyle.copyWith(fontWeight: FontWeight.w600),),
+                    subtitle:  Row(
+                      mainAxisSize: MainAxisSize.min,
+                      spacing: 10,
+                      children: [
+                        Expanded(child: Text(book.author)),
+                        Container(
+                            padding: EdgeInsets.all(3),
+                            decoration: BoxDecoration(
+                                color: AppColors.textFieldFillColor.withValues(alpha: 0.7),
+                                borderRadius: BorderRadius.circular(99)
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                              child: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                spacing: 4,
+                                children: [
+                                  Icon(Icons.access_time_outlined, size: 18,),
+                                  Text(book.time, style: AppTextStyles.smallTextStyle.copyWith(fontSize: 12, fontWeight: FontWeight.w600),)
+                                ],
                               ),
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(horizontal: 5.0),
-                                child: Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  spacing: 4,
-                                  children: [
-                                    Icon(Icons.access_time_outlined, size: 18,),
-                                    Text(book.time, style: AppTextStyles.smallTextStyle.copyWith(fontSize: 12, fontWeight: FontWeight.w600),)
-                                  ],
-                                ),
-                              )),
-                        ],
-                      ),
+                            )),
+                      ],
                     ),
-                  );
-                }).toList()
-
-            )
-          ],
+                  ),
+                );
+              })
+            ],
+          ),
         );
       },
     );
