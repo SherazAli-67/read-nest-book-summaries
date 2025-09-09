48784ed9faa380925b07d24d447372d038e3832e
SherazAli-67 <soomrosheraz054@gmail.com>
2025-09-09 13:09:14 +0500

    Authors Spotlight Page: UI Development 1. Browse authors section completed

--- lib/src/features/main_menu/search_page/authors_spotlight_page.dart
+++ lib/src/features/main_menu/search_page/authors_spotlight_page.dart
@@ -1,5 +1,7 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:read_nest/src/res/app_colors.dart';
+import 'package:read_nest/src/res/app_icons.dart';
 import 'package:read_nest/src/res/app_textstyle.dart';
 
 class AuthorsSpotLightPage extends StatelessWidget{
@@ -20,6 +22,7 @@ class AuthorsSpotLightPage extends StatelessWidget{
         padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10),
         child: Column(
           spacing: 20,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             ListTile(
               tileColor: AppColors.textFieldFillColor,
@@ -34,7 +37,95 @@ class AuthorsSpotLightPage extends StatelessWidget{
                   Text("Authors", style: AppTextStyles.smallTextStyle,)
                 ],
               ),
-            )
+            ),
+            Text("Browse Authors", style: AppTextStyles.regularTextStyle,),
+            Expanded(child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (_, index){
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Material(
+                  elevation: 1.0,
+                  shadowColor: Colors.white,
+                  child: ListTile(
+                    tileColor: Colors.white,
+                    titleAlignment: ListTileTitleAlignment.top,
+                    title: Text("James Clear", style: AppTextStyles.smallTextStyle.copyWith(fontWeight: FontWeight.w600),),
+                    leading: CircleAvatar(
+                      backgroundColor: AppColors.textFieldFillColor.withValues(alpha: 0.3),
+                      backgroundImage: CachedNetworkImageProvider(AppIcons.dummyBookImageUrl2),
+                    ),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      spacing: 5,
+                      children: [
+                        Container(
+                            padding: EdgeInsets.all(3),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(99),
+                                color: AppColors.textFieldFillColor
+                            ),
+                            child: Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                                child: Text('Habits & Productivity', style: AppTextStyles.smallTextStyle.copyWith(fontSize: 12, fontWeight: FontWeight.w600),)
+                            )),
+
+                        Text("Expert on habits, decision making, and continuous improvement. His work has been featured in major publications worldwide.", maxLines: 2, overflow: TextOverflow.ellipsis, style: AppTextStyles.smallTextStyle,),
+                        Row(
+                          children: [
+                            Expanded(child: Row(
+                              spacing: 10,
+                              children: [
+                                Icon(Icons.menu_book_rounded, color: Colors.grey,),
+                                Text('1 book', style: TextStyle(fontSize: 12, color: Colors.grey),)
+                              ],)),
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(2),
+                              child: CachedNetworkImage(
+                                imageUrl: AppIcons.dummyBookImageUrl2,
+                                height: 22,
+                                width: 16,
+                                fit: BoxFit.cover,
+                                placeholder: (context, url) => Container(
+                                  height: 22,
+                                  width: 16,
+                                  color: Colors.grey[300],
+                                  child: Icon(
+                                    Icons.book,
+                                    size: 12,
+                                    color: Colors.grey[600],
+                                  ),
+                                ),
+                                errorWidget: (context, url, error) => Container(
+                                  height: 22,
+                                  width: 16,
+                                  color: Colors.grey[300],
+                                  child: Icon(
+                                    Icons.book,
+                                    size: 12,
+                                    color: Colors.grey[600],
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                    trailing: SizedBox(
+                      width: 50,
+                      child: Row(
+                        spacing: 5,
+                        children: [
+                          Icon(Icons.person, color: Colors.grey, size: 15,),
+                          Text("2.1M",style: TextStyle(fontSize: 10, color: Colors.grey),)
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            }))
           ],
         ),
       )),
